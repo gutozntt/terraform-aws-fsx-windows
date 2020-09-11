e6b916ae28e8615df00fdd89ab6d2108b9e0944f
Gustavo Zanotto <gzanotto@reliam.com>
2020-09-10 23:15:17 -0700

    Pushing FSx module

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "arn" {
+  value = aws_fsx_windows_file_system.fsx.arn
+}
+output "dns_name" {
+  value = aws_fsx_windows_file_system.fsx.dns_name
+}
+output "id" {
+  value = aws_fsx_windows_file_system.fsx.id
+}
